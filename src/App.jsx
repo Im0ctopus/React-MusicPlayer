8a80ad2a543295881f85755c7be1d11b11243072
Leo <benemax2001@gmail.com>
2023-10-30 19:45:39 +0000

    Buttons working. Slide start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,86 @@
 import React, { useEffect, useMemo, useState } from "react"
-import { musicLogic } from "../hooks/MusicLogic"
-import Buttons from "./components/Buttons"
 
 function App() {
+  const [music, setMusics] = useState([
+    {
+      name: "Fato treino do City",
+      singer: "Sippinpurp",
+      file: "/Music/Sippinpurpp  Fato treino do City.mp3"
+    },
+    {
+      name: "FAKE LOVE",
+      singer: "Lon3r Johny",
+      file: "/Music/FAKE LOVE.mp3"
+    },
+    {
+      name: "Lamborghini",
+      singer: "KSI",
+      file: "/Music/KSI  Lamborghini Explicit ft P Money.mp3"
+    },
+  ])
+
+  const [currentSong, setCurrentSong] = useState(0);
+  const [playingSong, setPlayingSong] = useState(new Audio(music[currentSong].file));
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [musicDuration, setMusicDuration] = useState(0);
+  const min = parseInt(musicDuration / 60);
+  const sec = parseInt(musicDuration - (min * 60));
+  const [minPlaying, setMinPlaying] = useState(playingSong.currentTime);
+
+  useEffect(() => {
+    setPlayingSong(new Audio(music[currentSong].file));
+    playingSong.preload = "metadata";
+  }, [currentSong]);
+
+  useEffect(() => {
+    if (isPlaying) playingSong.play();
+    playingSong.onloadedmetadata = () => {
+      setMusicDuration(playingSong.duration);
+    };
+  }, [playingSong]);
+
+
+
+
+  function handlePlay() {
+    if (isPlaying) {
+      playingSong.pause();
+      setIsPlaying(false);
+    } else {
+      playingSong.play();
+      setIsPlaying(true);
+    }
+  }
+
+  function handleBack() {
+    setIsPlaying(true);
+    if (playingSong.currentTime <= 2) {
+      playingSong.pause();
+      if (currentSong == 0) setCurrentSong((music.length - 1))
+      else setCurrentSong(currentSong - 1);
+    } else playingSong.currentTime = 0;
+  }
+
+  function handleNext() {
+    setIsPlaying(true);
+    playingSong.pause();
+    if (currentSong == (music.length - 1)) setCurrentSong(0)
+    else setCurrentSong(currentSong + 1);
+  }
+
 
   return (
     <>
-      <Buttons></Buttons >
-
+      <div className="buttons">
+        <a onClick={handleBack}>back</a>
+        <a onClick={handlePlay}>play</a>
+        <a onClick={handleNext}>next</a>
+      </div>
+      <div className="Slider">
+        <input type="range" min={0} max={musicDuration} />
+        <p>{minPlaying}</p>
+        <p>{min}:{sec}</p>
+      </div>
     </>
   )
 }
